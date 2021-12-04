201dd005923cbf8e7d9bed24c808907a481dd132
mfbx4jpb <mfbx4jpb@B-LOSX6QXL410.lan>
2021-12-04 14:26:43 +0000

    Defend(), numberRandomiser(), optionSelect(), whoGoesFirst()  Implemented and Tested

--- __test__/battle.test.js
+++ __test__/battle.test.js
@@ -10,6 +10,7 @@ const { Battle } = require("../sourceFiles/battle.js");
 const { describe, expect } = require("@jest/globals");
 
 describe("Test suite for Battle", () => {
+	let functionSpy = null;
   test("Battle accepts two trainers", () => {
     const trainer1 = new Trainer("Ash");
     const trainer2 = new Trainer("Brock");
@@ -29,12 +30,63 @@ describe("Test suite for Battle", () => {
       } else zero++;
     }
     //assert
-    console.log((zero/1000)*100 + " %"); //returns ~50%
+    console.log((zero / 1000) * 100 + " %"); //returns ~50%
   });
-    test('optionSelect should give a list of actions to the player ', () => {
-        testBattle = new Battle();
-        //act 
-        testBattle.optionSelect();
-    });
+  test("optionSelect() should give a list of actions to the player ", () => {
+    testBattle = new Battle();
+    //act
+    testBattle.optionSelect();
+  });
+  test("optionSelect() should return user input selection", () => {
+    testBattle = new Battle();
+    //act
+    //assert
+    expect(testBattle.optionSelect(1)).toEqual(1);
+  });
+  test("optionSelect() should return only 1 to 5", () => {
+    //act
+    testBattle = new Battle();
+    //act
+    //assert
+    expect(testBattle.optionSelect(1)).toEqual(1);
+    expect(testBattle.optionSelect(5)).toEqual(5);
+    //infinite loop testing - can be seen by running testBattle.optionSelect(0) - robust way to test this?
 
+  });
+  test("whoGoesFirst() should swap trainers if numberRandomiser() is false", () => {
+    //act
+    const trainer1 = new Trainer("Ash");
+    const trainer2 = new Trainer("Brock");
+		testBattle = new Battle(trainer1, trainer2);
+		//mock function setup
+		functionSpy = jest.spyOn(testBattle, 'numberRandomiser');
+		functionSpy.mockReturnValue(false);
+		testBattle.whoGoesFirst();
+		expect(testBattle.player1.name).toEqual("Brock");
+		expect(testBattle.player2.name).toEqual("Ash");
+		functionSpy.mockRestore();
+  });
+	test("whoGoesFirst() should NOT swap trainers if numberRandomiser() is true", () => {
+    //act
+    const trainer1 = new Trainer("Ash");
+    const trainer2 = new Trainer("Brock");
+		testBattle = new Battle(trainer1, trainer2);
+		//mock function setup
+		functionSpy = jest.spyOn(testBattle, 'numberRandomiser');
+		functionSpy.mockReturnValue(true);
+		testBattle.whoGoesFirst();
+		expect(testBattle.player1.name).toEqual("Ash");
+		expect(testBattle.player2.name).toEqual("Brock");
+		functionSpy.mockRestore();
+  });
+	test('Calling defend should set current trainer defend flag to true', () => {
+		const trainer1 = new Trainer("Ash");
+    const trainer2 = new Trainer("Brock");
+		trainer1.trainerInitialisation();
+		trainer2.trainerInitialisation();
+		testBattle = new Battle(trainer1, trainer2);
+		testBattle.defend(trainer1);
+		expect(testBattle.player1.defendStatus).toEqual(true);
+		expect(testBattle.player2.defendStatus).toEqual(false);
+	});
 });
--- sourceFiles/battle.js
+++ sourceFiles/battle.js
@@ -1,4 +1,3 @@
-const { buildFailureTestResult } = require("@jest/test-result");
 const {
   Pokemon,
   pokemonData,
@@ -12,12 +11,13 @@ function Battle(trainer1, trainer2) {
         this.endGame = false;
         this.player1 = trainer1;
         this.player2 = trainer2;
-        this.winner = false;
-        
-
+        this.winner = null
+    
 }
 
 Battle.prototype.gameLoop = function(){
+
+    this.whoGoesFirst();
 
     while(this.endGame==false){
 
@@ -27,7 +27,16 @@ Battle.prototype.gameLoop = function(){
 
 }
 
-
+Battle.prototype.whoGoesFirst = function () {
+    let firstPlayer = this.numberRandomiser();
+    if(firstPlayer===false){
+        console.log("numberRandomiser() returned false - player 2 will go frst!");
+        let temp = this.player2;
+        this.player2 = this.player1;
+        this.player1 = temp;
+    }
+    console.log(`${this.player1.name} will go first.`);
+  }
 
 //Used to determine which player goes first and whether critical hit
 Battle.prototype.numberRandomiser = function () {
@@ -37,10 +46,16 @@ Battle.prototype.numberRandomiser = function () {
   }
 
   //selection variable for testing purposes; to be overriden with UI
-  Battle.prototype.optionSelect = function (selection) {
-      console.log("Please choose an action: \n\n 1. Attack \n 2. Defend \n 3. Switch Pokemon \n 4. Catch Pokemon \n 5. Remove Pokemon ");
-    
-  }
+  Battle.prototype.optionSelect = function (selection=1) {
+    let invalidSelection = true;
+    while(invalidSelection){  
+    console.log("Please choose an action: \n\n 1. Attack \n 2. Defend \n 3. Switch Pokemon \n 4. Catch Pokemon \n 5. Remove Pokemon ");
+      //USER INPUT - SELECTION
+      if(selection >= 1 && selection <=5){
+          invalidSelection=false;
+      } else "Invalid Input."}
+      return selection;}
+  
 
   Battle.prototype.attack = function () {
 
@@ -53,9 +68,9 @@ Battle.prototype.numberRandomiser = function () {
         //if playerdefeated = true, break, change flag;
   }
 
-  Battle.prototype.defend = function () {
-
-
+  Battle.prototype.defend = function (trainer) {
+    console.log(`Your current Pokemon ${trainer.pokemonInventory[trainer.currentPokemon].name} is now defending!`);  
+    trainer.defendStatus=true;
   }
 
  Battle.prototype.switchPokemon = function () {
